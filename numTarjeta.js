4106978d277b3c78653114642b033d153eadcd91
Alvaro Llamas <alvaro.llamas@gmail.com>
2024-10-08 14:09:03 +0200

    Tercer commit

--- numTarjeta.js
+++ numTarjeta.js
@@ -7,9 +7,12 @@
     */
 
     // Función que verifica si el número de 16 dígitos es correcto
-    function verificarNumeroTarjeta($numero) {
+    function verificarNumeroTarjeta(numero) {
         
         // Declaración de variables
-        let numeros = $numero.slice(0,15); // Separamos en dígitos únicos
-        
+        let numeros = numero.slice(0,15); // Separamos en dígitos únicos
+
     }
+
+    // Llamamos a la función y le pasamos un número
+    verificarNumeroTarjeta(4539148803436467);
