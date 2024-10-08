df13ac34968006f7ac7af3cc6b5643d4e499024a
Alvaro Llamas <alvaro.llamas@gmail.com>
2024-10-08 13:41:26 +0200

    Primer commit

---
+++ numTarjeta.js
@@ -0,0 +1,11 @@
+// Álvaro Llamas Huerta
+
+    /*
+    Queremos verificar si un número de 16 dígitos introducido por el usuario es un número
+    válido de tarjeta de crédito. Para ello, investiga sobre el algoritmo de Luhn.
+    */
+
+    // Función que verifica si el número de 16 dígitos es correcto
+    function verificarNumeroTarjeta($numero) {
+        
+    }
